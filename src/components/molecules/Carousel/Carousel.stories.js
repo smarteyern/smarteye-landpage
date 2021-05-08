9cae753c52b12a3cd670baa6aed69367ed282c74
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-08 18:21:41 -0300

    docs: add component document

---
+++ src/components/molecules/Carousel/Carousel.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Carousel from "./Carousel";
+
+export default {
+  title: "Components/Molecules/Carousel",
+  component: Carousel,
+};
+
+export const usage = () => <Carousel></Carousel>;
