8506f92b0459084a0297725b9b3dcc9efc8f4e0f
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-24 22:44:53 -0300

    feat: styles pages change

--- src/components/pages/style.js
+++ src/components/pages/style.js
@@ -1,3 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const ListIcon = styled.div`
+  display: inline-block;
+
+  p,
+  li {
+    font-size: 16px;
+    font-weight: 300;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  li {
+    &::before {
+      content: '\\2713\\0020';
+      color: ${props => props.theme.colors.primary.light};
+    }
+  }
+`;
